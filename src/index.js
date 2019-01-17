bb1d9f7b813ec20c9f57c6ab98632105b86239e0
Alex Nelson <alexsnelson95@gmail.com>
2019-01-17 15:05:56 -0800

    board props

--- src/index.js
+++ src/index.js
@@ -149,7 +149,8 @@ slack.on('/get-tickets', (msg, bot) => {
 
     jira.board.getBoard({boardId: "57"}, function(error, boardInfo) {
       console.log("binfo: " + boardInfo);
-    }
+    });
+
     jira.board.getIssuesForBoard(opts, function(error, issues) {
       if (error) {
         console.log("error");
